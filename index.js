bc463b8c0b26c4c0bf0a07e89fb6b2019d977084
Tessyrich <olyrich3@gmail.com>
2023-11-13 15:47:28 +0100

    change background

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ let count = 0;
 let countUl = document.getElementById("count-ul");
 let saveEl = document.getElementById("save-el");
 const InCrement = () => {
-  count = count + 1;
-  countUl.innerText = count;
+  count += 1;
+  countUl.textContent = count;
 };
 
 const Save = () => {
-  countNum = count + "-";
-  saveEl.innerText = countNum;
-  console.log(count);
+  let countNum = count + " - ";
+  saveEl.textContent += countNum;
+  console.log(countNum);
+  countUl.textContent = 0;
+  count = 0;
 };
-Save();
