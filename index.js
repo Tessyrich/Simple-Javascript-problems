14d22fe4e8854018d589d6ee136c27eab1a1a061
Tessyrich <olyrich3@gmail.com>
2023-11-17 15:37:08 +0100

    solve more javascript problems

--- index.js
+++ index.js
@@ -13,3 +13,33 @@ const Save = () => {
   countUl.textContent = 0;
   count = 0;
 };
+let errormsg = document.getElementById("error");
+render = () => {
+  errormsg.textContent = "something went wrong please try again";
+};
+
+let num1 = 8;
+let num2 = 2;
+document.getElementById("num1").textContent = num1;
+document.getElementById("num2").textContent = num2;
+let sum = document.getElementById("sum-el");
+
+const Add = () => {
+  let numSum = num1 + num2;
+  sum.textContent = "Sum:" + numSum;
+};
+const Substract = () => {
+  let numSum = num1 - num2;
+  sum.textContent = "Sum:" + numSum;
+};
+const Divide = () => {
+  let numSum = num1 / num2;
+  sum.textContent = "Sum:" + numSum;
+};
+const Multiply = () => {
+  let numSum = num1 * num2;
+  sum.textContent = "Sum:" + numSum;
+};
+let firstCard = 10;
+let secondCard = 4;
+let sumCard = firstCard + secondCard;
