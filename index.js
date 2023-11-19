632b174206dc1fe21f67192f750489b4abddbeb8
Tessyrich <olyrich3@gmail.com>
2023-11-19 22:44:14 +0100

    Building a blackjack game

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let num1 = 8;
 let num2 = 2;
 document.getElementById("num1").textContent = num1;
 document.getElementById("num2").textContent = num2;
-let sum = document.getElementById("sum-el");
+let sum = document.getElementById("sum-ell");
 
 const Add = () => {
   let numSum = num1 + num2;
@@ -40,6 +40,34 @@ const Multiply = () => {
   let numSum = num1 * num2;
   sum.textContent = "Sum:" + numSum;
 };
+// BLACKJACK GAME
 let firstCard = 10;
-let secondCard = 4;
+let secondCard = 21;
 let sumCard = firstCard + secondCard;
+let hasBlackcard = false;
+let isAlive = true;
+let message = " ";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
+
+startGame = () => {
+  cardEl.textContent = "Card:" + firstCard + " " + secondCard;
+  sumEl.textContent = "Sum:" + sumCard;
+  if (sumCard <= 20) {
+    message = "Do you want to draw a new card";
+  } else if (sumCard === 21) {
+    message = "whoo you have a blackcard";
+    hasBlackcard = true;
+  } else {
+    message = "you are out of the game";
+  }
+  messageEl.textContent = message;
+};
+
+newCard = () => {
+  console.log(" drawing a new card from the deck");
+  let Card = 5;
+  sumCard += Card;
+  startGame();
+};
